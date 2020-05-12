80c8b027d9d48b6f990c86e7020c00df7b6fd7c3
CelineRondeau <rondeau9314@gmail.com>
2020-05-12 19:17:16 -0400

    Made some changes to the route file

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
   # CRUD Actions according to conventions
 
   # view all tasks
-  get "/tasks", to: "tasks#index", as: :tasks
+  get "/tasks", to: "tasks#index"
+
+  post "tasks", to: "tasks#create"
+
+  # create tasks
+  get "tasks/new", to: "tasks#new", as: :new_task
 
   # view details of a specific restaurant
-  get "/tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
