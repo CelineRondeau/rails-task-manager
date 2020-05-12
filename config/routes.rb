12d5697c2631080196befbc68d269100186bdfb5
CelineRondeau <rondeau9314@gmail.com>
2020-05-12 14:00:26 -0400

    Working on the route for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD Actions according to conventions
+
+  # view all tasks
+  get "/tasks", to: "tasks#index", as: :tasks
 end
